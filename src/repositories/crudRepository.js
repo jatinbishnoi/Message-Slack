8f772c7ca69e1ec01526891671ec6c09710bd823
Jatin Bishnoi <jatinkumar160902@gmail.com>
2024-12-10 20:57:42 +0530

    added workspaces api on 16 nov

--- src/repositories/crudRepository.js
+++ src/repositories/crudRepository.js
@@ -22,6 +22,14 @@ export default function crudRepository(model) {
           new: true
         });
         return updatedDoc;
+      },
+      deleteMany: async function( modelIds) {
+        const response = await model.deleteMany({
+          _id: {
+            $in : modelIds
+          }
+        });
+        return response;
       }
     };
   }
